df6b6729b42fdd2b343f91244d5721e02a3a4819
Evgeny Medvedev <medvedev1088@gmail.com>
2018-02-28 17:11:45 +0700

    Fix exporter KeyError

--- ethereumScraper/pipelines.py
+++ ethereumScraper/pipelines.py
@@ -5,7 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import CsvItemExporter
-    
+
+
 class EthereumScraperExportPipeline(object):
 
     def open_spider(self, spider):
@@ -16,20 +17,21 @@ class EthereumScraperExportPipeline(object):
             exporter.finish_exporting()
 
     def _exporter_for_item(self, item):
-        type = item['type']
-        if type not in self.item_type_to_exporter:
-            if type == 'b':
+        item_type = item.get('type', None)
+        if item_type is not None and item_type not in self.item_type_to_exporter:
+            if item_type == 'b':
                 f = open('blocks.csv', 'wb')
-            elif type == 't':
+            elif item_type == 't':
                 f = open('transactions.csv', 'wb')
             else:
                 f = open('unknown.csv', 'wb')
             exporter = CsvItemExporter(f)
             exporter.start_exporting()
-            self.item_type_to_exporter[type] = exporter
-        return self.item_type_to_exporter[type]
+            self.item_type_to_exporter[item_type] = exporter
+        return self.item_type_to_exporter.get(item_type, None)
 
     def process_item(self, item, spider):
         exporter = self._exporter_for_item(item)
-        exporter.export_item(item)
+        if exporter is not None:
+            exporter.export_item(item)
         return item
