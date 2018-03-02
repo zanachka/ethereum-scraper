d4620a61755dbaebf3a06e47f416c190149c9ea4
Evgeny Medvedev <medvedev1088@gmail.com>
2018-03-02 23:36:30 +0700

    Add test for debug_traceTransaction

--- eth_tx_trace_processor/main.py
+++ eth_tx_trace_processor/main.py
@@ -1,4 +1,18 @@
-from ethjsonrpc import EthJsonRpc
-c = EthJsonRpc('infuranet.infura.io/eQq8vdX3vS511ulZw8fr', 443, tls=True)
-bn = c.eth_getBlockByNumber(block=0)
-print bn
+import json
+
+import requests
+
+headers = {'Content-Type': 'application/json'}
+
+data = {
+            'jsonrpc': '2.0',
+            'method':  'debug_traceTransaction',
+            'params':  ['0x663254ac1888a6f04e3330c4c6b86127f618b486be6ce85cfda6f500fa1c2849', {}],
+            'id':      1,
+        }
+
+response = requests.Session().post('https://mainnet.infura.io/eQq8vdX3vS511ulZw8fs',
+                        headers = headers,
+                        data=json.dumps(data))
+
+print response.body_as_unicode()
